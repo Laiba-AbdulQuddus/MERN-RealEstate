bc1a99b69f9bb6ec890e35327fbe7be1ab93c6ac
Laiba Abdul Quddus <laibaabdulquddus@gmail.com>
2025-05-31 23:04:36 +0500

    Create and Test API Route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
 dotenv.config();
+
+console.log('MONGO URL:', process.env.MONGO);
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('MongoDB connected!');
@@ -11,5 +15,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!1');
+    console.log('Server is running on port 3000!');
 });
+
+app.use("/api/user", userRouter); 
